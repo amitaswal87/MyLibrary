c088765557f0aab46159d88a3e43ebc030823792
Amit <amitaswal87@gmail.com>
2024-08-02 14:39:40 +0530

    first commit

---
+++ Sources/MyLibrary/Demo.swift
@@ -0,0 +1,15 @@
+//
+//  Demo.swift
+//
+//
+//  Created by apple  on 02/08/24.
+//
+
+import Foundation
+
+public class Demo{
+    
+    public static func printLog(log : String){
+        print("Printing Log :: ",log)
+    }
+}
